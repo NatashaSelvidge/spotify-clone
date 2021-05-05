f725166ded9f2f12dd90a7135783719f261a8600
Natasha Selvidge <Natasha.selvidge@gmail.com>
2021-05-05 14:01:12 -0700

    set the state token and used useEffect to hide the token and setToken state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React from "react"
-import './App.css';
-import Login from './Login';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Login from "./Login";
+import { getTokenFromUrl } from "./spotify";
 
 function App() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+    }
+    console.log("This is my token 👉 ", token);
+  }, []);
+
   return (
     <div className="app">
-     <h1>Hello World</h1>
-     {/* {Spotify Logo} */}
-     {/* {Login with Spotify Button} */}
-     <Login/>
-     {/* {} */}
+      {token ? <h1>I am logged in</h1> : <Login />}
+      {/* {} */}
     </div>
   );
 }
