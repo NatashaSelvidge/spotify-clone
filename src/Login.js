0dcdf6f9706a49fb7e3864b8577d5a8dda2e4569
Natasha Selvidge <Natasha.selvidge@gmail.com>
2021-05-05 13:59:07 -0700

    Login button with url

--- src/Login.js
+++ src/Login.js
@@ -1,14 +1,18 @@
-import React from 'react'
-import "./Login.css"
+import React from "react";
+import "./Login.css";
+import { loginUrl } from "./spotify";
 
 function Login() {
   return (
     <div className="login">
       <h1>Login Page</h1>
-       {/* {Spotify Logo} */}
-     {/* {Login with Spotify Button} */}
+      <img
+        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+        alt="spotify logo"
+      />
+      <a href={loginUrl}>LOGIN WITH SPOTIFY</a>
     </div>
-  )
+  );
 }
 
 export default Login;
