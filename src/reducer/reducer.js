25ecf0c0997bbd59bc2fafb460a11bfeb36ebc0c
Natasha Selvidge <Natasha.selvidge@gmail.com>
2021-05-10 14:06:36 -0700

    added set user and set token to reducer
    I added temp token for development purposes.

---
+++ src/reducer/reducer.js
@@ -0,0 +1,29 @@
+export const initialState = {
+  user: null,
+  playlist: [],
+  playing: false,
+  item: null,
+  //REMOVE AFTER DEVELOPMENT
+  token:
+    "BQA1v1iadscsk_NzJuy7hRqMlB7cetFqrZ852qMQBdrYZ_fZ-RiAlexAAJuxmfJkxjyRJAOkXuhcO00Qwu6uXjU0PePMup1dINrez2iz5MPj90D42FDqm8Bzvpe6QULZag19ZC50CoWj7FkxnHuDa0l-Nul87Xt2wA",
+};
+
+const reducer = (state, action) => {
+  console.log(action);
+
+  switch (action.type) {
+    case "SET_USER":
+      return {
+        ...state,
+        user: action.user,
+      };
+    case "SET_TOKEN":
+      return {
+        ...state,
+        token: action.token,
+      };
+    default:
+      return state;
+  }
+};
+export default reducer;
