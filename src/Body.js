2fe7752acd4a93a507db7c591dc54fe53728e549
Natasha Selvidge <Natasha.selvidge@gmail.com>
2021-05-14 14:28:14 -0700

    Added song list of body, styling, and icons

--- src/Body.js
+++ src/Body.js
@@ -1,9 +1,11 @@
-import { Favorite, PlayCircleFilled, MoreHoriz } from "@material-ui/icons";
 import React from "react";
 import "./Body.css";
 import { useDataLayerValue } from "./DataLayer";
 import Header from "./Header";
-import Song from "./Song";
+import SongRow from "./SongRow";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import PlayCircleFilledIcon from "@material-ui/icons/PlayCircleFilled";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 
 function Body({ spotify }) {
   const [{ discover_weekly }, dispatch] = useDataLayerValue();
@@ -17,6 +19,16 @@ function Body({ spotify }) {
           <h2>Discover Weekly</h2>
           <p>{discover_weekly?.description}</p>
         </div>
+      </div>
+      <div className="body__songs">
+        <div className="body__icons">
+          <PlayCircleFilledIcon className="body__shuffle" />
+          <FavoriteIcon fontSize="large" />
+          <MoreHorizIcon />
+        </div>
+        {discover_weekly?.tracks.items.map((item) => {
+          <SongRow track={item.track} />;
+        })}
       </div>
     </div>
   );
